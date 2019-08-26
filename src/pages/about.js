1f6ab50a5932a645388b33e64c99047cb1f2fdc8
Baraghen <samir.blom@gmail.com>
2019-08-26 14:54:06 +0200

    Ny bakgrund?

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from "react"
 import Header from "../components/header"
 export default () => (
   <div style={{ color: `teal` }}>
-    <Header headerText="About Gatsby" />
+    <Header headerText="This is my Gatsby Site" />
     <Header headerText="It's pretty cool!" />
     <p>Cool beans.</p>
   </div>
