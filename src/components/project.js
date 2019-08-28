dcd153ac1bb44e3c6304595a30bc29e1ed30708b
Baraghen <samir.blom@gmail.com>
2019-08-28 23:44:07 +0200

    glömde några filer

---
+++ src/components/project.js
@@ -0,0 +1,13 @@
+import React from "react"
+import styles from "../components/index.module.css"
+
+export default props => (
+    <div className={styles.project}>
+      <img src={props.thumbnail} className={styles.thumbnail} alt="" />
+      <div className={styles.description}>
+        <h3 className={styles.projectname}>{props.projectname}</h3>
+        <p className={styles.excerpt}>{props.excerpt}</p>
+        <a className={styles.projectlink} href={props.projectlink} target="_blank">{props.projectlinktext}</a>
+      </div>
+    </div>
+) 
